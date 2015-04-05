47ce0f1ebbdd26cf9c894adabfd99e87f324022c
natsu90 <solomon_scorpio90@yahoo.com>
2015-04-05 16:42:16 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+require 'vendor/autoload.php';
+use Symfony\Component\Process\Process;
+
+$app = new \Slim\Slim();
+
+$app->get('/', function () use($app) {
+    $app->render('doc.php');
+});
+
+$app->get('/api/v1/:query_type/:query_value', function ($query_type, $query_value) use($app) {
+
+    $process = new Process(sprintf('casperjs gst.proc %s %s', $query_type, $query_value));
+	$process->run();
+
+	if (!$process->isSuccessful()) {
+    	throw new \RuntimeException($process->getErrorOutput());
+	}
+
+	$response = $app->response();
+	$response['Content-Type'] = 'application/json';
+	$response->status(200);
+	$response->body(json_encode(array('results' => json_decode($process->getOutput())), JSON_PRETTY_PRINT));
+});
+
+$app->run();
---
+++ templates/doc.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Malaysia Good and Service Tax (GST) Status Lookup API</title>
+</head>
+<body><a href="https://github.com/natsu90/gst-api"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://camo.githubusercontent.com/e7bbb0521b397edbd5fe43e7f760759336b5e05f/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f677265656e5f3030373230302e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_green_007200.png"></a>
+<h2>Malaysia Good and Service Tax (GST) Status Lookup API</h2>
+<p>// get by GST number <br> # GET <a href="/api/v1/gst_no/000940552192">/api/v1/gst_no/:gst_no</a></p>
+<p>// get by Business Registration Number<br> # GET <a href="/api/v1/reg_no/1058531-W">/api/v1/reg_no/:reg_no</a></p>
+<p>// search by Business Name <br> # GET <a href="/api/v1/name/7-eleven">/api/v1/name/:name</a></p>
+</body>
+</html>
