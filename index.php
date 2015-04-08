1447bb400bb555c321e44765b13c1330b8a1ddf9
natsu90 <solomon_scorpio90@yahoo.com>
2015-04-09 02:01:58 +0800

    add file cache

--- index.php
+++ index.php
@@ -1,13 +1,62 @@
 <?php
 require 'vendor/autoload.php';
 use Symfony\Component\Process\Process;
-use Desarrolla2\Cache\Cache;
 use Desarrolla2\Cache\Adapter\File;
+use Desarrolla2\Cache\Cache as CacheAdapter;
+ 
+class Cache extends \Slim\Middleware
+{
+	protected $cache;
+ 
+    public function __construct(CacheAdapter $cache)
+    {
+        $this->cache = $cache;
+    }
+ 
+    public function call()
+    {
+		$uri = $this->app->request()->getResourceUri();
+		$rsp = $this->app->response();
+        $key = urlencode($uri);
+		
+        $data = $this->fetch($key);
+        if ($data) {
+            
+            $rsp["Content-Type"] = $data['content_type'];
+            $rsp->body($data['body']);
+            return;
+        }
+ 
+        $this->next->call();
+ 
+        if ($rsp->status() == 200) {
+            
+            $ttl = 3600;
+            if(preg_match("/gst_no/", $uri) && preg_match("/results/", $rsp->body()))
+				$ttl = null;
 
-$adapter = new File("tmp");
+            $this->save($key, $rsp["Content-Type"], $rsp->body(), $ttl);
+        }
+    }
+ 
+    protected function fetch($key)
+    {
+        $cache = $this->cache->get($key);
+        return unserialize($cache);
+    }
+ 
+    protected function save($key, $contentType, $body, $ttl)
+    {
+    	$cache = array('content_type' => $contentType, 'body' => $body);
+        $cache = serialize($cache);
+        $this->cache->set($key, $cache, $ttl);
+    }
+}
+
+$adapter = new File("cache");
 
 $app = new \Slim\Slim();
-$app->add(new Slim\Middleware\Cache(new Cache($adapter)));
+$app->add(new Cache(new CacheAdapter($adapter)));
 
 $app->get('/', function () use($app) {
     $app->render('doc.php');
